bbb15023113b4f8b80a791480e82984ec3a9f898
renekrizak <rene.krizak@gmail.com>
2022-05-18 20:32:47 +0200

    stuff

--- Server/Server.cs
+++ Server/Server.cs
@@ -8,14 +8,45 @@ using System.Net.Sockets;
 using Dapper;
 using System.Data.SQLite;
 using System.Security.Cryptography;
+using System.Configuration;
 
-/* UserDB Structure
+/*
+ ---------------------------------
+        Server DB structure
+ ---------------------------------
+ */
 
+/* CREATE TABLE "User" (
+	"UserID"	INTEGER NOT NULL UNIQUE,
+	"Username"	TEXT NOT NULL UNIQUE,
+	"Password"	TEXT NOT NULL,
+	"Email"	TEXT NOT NULL,
+	PRIMARY KEY("UserID" AUTOINCREMENT)
 );
  */
 
-/* Room logs structure
- 
+/*CREATE TABLE "Room" (
+	"RoomID"	TEXT NOT NULL UNIQUE,
+	"RoomPWD"	INTEGER NOT NULL,
+	PRIMARY KEY("RoomID")
+);
+*/
+
+/*CREATE TABLE "UserRooms" (
+	"UserID"	INTEGER NOT NULL,
+	"RoomID"	TEXT NOT NULL,
+	FOREIGN KEY("UserID") REFERENCES "User"("UserID")
+);
+ */
+
+/*CREATE TABLE "RoomLogs" (
+	"UserID"	INTEGER NOT NULL,
+	"Time"	TEXT NOT NULL,
+	"Message"	TEXT NOT NULL,
+	"RoomID"	TEXT NOT NULL,
+	FOREIGN KEY("RoomID") REFERENCES "Room"("RoomID"),
+	FOREIGN KEY("UserID") REFERENCES "User"("UserID")
+);
  */
 
 
@@ -23,7 +54,11 @@ namespace Server
 {
      class Server
     {
-        static void Main(string[] args)
+		private static string LoadConnectionString(string id = "Default")
+		{
+			return ConfigurationManager.ConnectionStrings[id].ConnectionString;
+		}
+		static void Main(string[] args)
         {
         }
     }
